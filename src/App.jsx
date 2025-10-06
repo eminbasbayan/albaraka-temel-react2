f60a2fdb33011d537271a79a65afb80bfa9d4d8a
Emin Başbayan <emin.basbayan@icloud.com>
2025-10-06 16:22:20 +0300

    react state mantığı

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-function App(){
-    return <h1>Hello React!</h1>
+import { useState } from 'react';
+
+function App() {
+  const [state, setState] = useState('Emree');
+  //   let name = 'Emre';
+
+  function ismiDegistir(parametre) {
+    setState(parametre);
+    // name = 'Ahmet';
+    // console.log(name);
+  }
+
+  console.log('component render oldu!');
+
+  return (
+    <div>
+      <h2>{state}</h2>
+      <button onClick={()=> ismiDegistir("hello")} className="">İsmi Değiştir</button>
+    </div>
+  );
 }
 
 export default App;
