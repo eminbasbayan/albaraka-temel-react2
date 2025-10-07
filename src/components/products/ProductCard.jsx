f60d600000c40f067f357e7a239ea2aa2bf4213d
Emin Başbayan <emin.basbayan@icloud.com>
2025-10-07 13:39:17 +0300

    local state olayı

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import './ProductCard.css';
 
 function ProductCard(props) {
+  const [titleState, setTitleState] = useState(props.title);
+
   return (
     <div className="product-card">
       <img src={props.image} alt={props.title} className="product-image" />
       <div className="product-info">
         <span className="product-category">{props.category}</span>
-        <strong className="product-title">{props.title}</strong>
+        <strong className="product-title">{titleState}</strong>
         <p className="product-description">{props.description}</p>
         <span className="product-price">₺{props.price}</span>
+
+        <button onClick={() => setTitleState('Yeni İsim!')}>
+          Ürün İsmini Değiştir!
+        </button>
       </div>
     </div>
   );
