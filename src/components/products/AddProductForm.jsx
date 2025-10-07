a35ce64c69ece15734d8612f532a56901bc96481
Emin Başbayan <emin.basbayan@icloud.com>
2025-10-07 15:33:46 +0300

    çoklu state ve çoklu function ile form yönetimi

--- src/components/products/AddProductForm.jsx
+++ src/components/products/AddProductForm.jsx
@@ -1,28 +1,75 @@
+import { useState } from 'react';
 import Button from '../ui/Button';
 import './AddProductForm.css';
 
 const AddProductForm = () => {
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleTitleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function handleImageChange(event) {
+    setImage(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    setCategory(event.target.value);
+  }
+
+  function handlePriceChange(event) {
+    setPrice(event.target.value);
+  }
+
+  function handleDescriptionChange(event) {
+    setDescription(event.target.value);
+  }
+
   return (
     <form className="add-product-form">
       <label>
-        Title:
-        <input type="text" placeholder="Bir ürün ismi yazınız..." />
+        Title: {title}
+        <input
+          type="text"
+          placeholder="Bir ürün ismi yazınız..."
+          onChange={handleTitleChange}
+        />
       </label>
       <label>
-        Image URL:
-        <input type="text" placeholder="Bir ürün görsel linki yazınız..." />
+        Image URL: {image}
+        <input
+          type="text"
+          placeholder="Bir ürün görsel linki yazınız..."
+          onChange={handleImageChange}
+        />
       </label>
       <label>
-        Price:
-        <input type="number" placeholder="Bir ürün fiyatı yazınız..." />
+        Price: {price}
+        <input
+          type="number"
+          placeholder="Bir ürün fiyatı yazınız..."
+          onChange={handlePriceChange}
+        />
       </label>
       <label>
-        Description:
-        <input type="text" placeholder="Bir ürün açıklaması yazınız..." />
+        Description: {description}
+        <input
+          type="text"
+          placeholder="Bir ürün açıklaması yazınız..."
+          onChange={handleDescriptionChange}
+        />
       </label>
       <label>
-        Category:
-        <input type="text" placeholder="Bir ürün kategorisi yazınız..." />
+        Category: {category}
+        <input
+          type="text"
+          placeholder="Bir ürün kategorisi yazınız..."
+          onChange={handleCategoryChange}
+        />
       </label>
 
       <Button color="success" size="lg">
