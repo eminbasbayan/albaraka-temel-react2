ae0214f1998830d6e244fa6a64221444bd5bd9ad
Emin Başbayan <emin.basbayan@icloud.com>
2025-10-07 14:54:34 +0300

    dinamik button yapımı

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Button from './components/ui/Button';
 function App() {
   return (
     <div>
-      <Button />
+      <Button color="primary" title="Sepete Ekle" disabled />
+      <Button color="danger" size="lg" title="Ürünü Sil" />
+      <Button color="success" size="sm" title="Yeni Ürün Oluştur" />
+      <Button link size="sm" title="Yeni Ürün Oluştur" />
       {/* <Products /> */}
     </div>
   );
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,11 +1,15 @@
-import "./Button.css";
+import './Button.css';
 
-const Button = () => {
-   
+const Button = (props) => {
+  const classNames = `btn btn-${props.color} ${props.link && 'btn-link'} btn-${
+    props.size
+  }`;
 
   return (
-    <button className="btn btn-danger btn-sm" disabled>Button</button>
-  )
-}
+    <button className={classNames} disabled={props.disabled}>
+      {props.title ? props.title : 'Button'}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
